e0aae1546b1c0390a618f1b8fec3616ee459658a
Hans Pabst <hans.pabst@intel.com>
2025-07-16 16:00:45 +0200

    Support build_type=Coverage.

--- repos/spack_repo/builtin/packages/cp2k/package.py
+++ repos/spack_repo/builtin/packages/cp2k/package.py
@@ -65,6 +65,11 @@ class Cp2k(MakefilePackage, CMakePackage, CudaPackage, ROCmPackage):
 
     generator("ninja")
 
+    variant("build_type", default="Release", description="CMake build type",
+        values=("Debug", "Release", "RelWithDebInfo", "MinSizeRel", "Coverage"),
+        when="build_system=cmake"
+    )
+
     variant("mpi", default=True, description="Enable MPI support")
     variant("openmp", default=True, description="Enable OpenMP support")
     variant(
--- repos/spack_repo/builtin/packages/dbcsr/package.py
+++ repos/spack_repo/builtin/packages/dbcsr/package.py
@@ -35,6 +35,11 @@ class Dbcsr(CMakePackage, CudaPackage, ROCmPackage):
     version("2.1.0", sha256="9e58fd998f224632f356e479d18b5032570d00d87b86736b6a6ac2d03f8d4b3c")
     version("2.0.1", sha256="61d5531b661e1dab043353a1d67939ddcde3893d3dc7b0ab3d05074d448b485c")
 
+    variant("build_type", default="Release", description="CMake build type",
+        values=("Debug", "Release", "RelWithDebInfo", "MinSizeRel", "Coverage"),
+        when="build_system=cmake"
+    )
+
     variant("tests", default=False, description="Build DBCSR unit tests")
     variant("tests", default=True, description="Build DBCSR unit tests", when="@2.1:2.2")
     variant("mpi", default=True, description="Compile with MPI")
