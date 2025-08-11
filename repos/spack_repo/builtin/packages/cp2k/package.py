b2641a78fda0d951d6dbee06c25d6c9e8be298c2
Hans Pabst <hans.pabst@intel.com>
2025-08-11 20:28:29 +0200

    cp2k/dbcsr: propagate +shared for libxsmm

--- repos/spack_repo/builtin/packages/cp2k/package.py
+++ repos/spack_repo/builtin/packages/cp2k/package.py
@@ -258,10 +258,10 @@ class Cp2k(MakefilePackage, CMakePackage, CudaPackage, ROCmPackage):
         depends_on("cray-libsci+openmp", when="^[virtuals=blas] cray-libsci")
 
     with when("smm=libxsmm"):
+        depends_on("libxsmm+shared")
         # require libxsmm-1.11+ since 1.10 can leak file descriptors in Fortran
         depends_on("libxsmm@1.11:")
         depends_on("libxsmm@1.17:", when="@9.1:")
-        depends_on("libxsmm+shared", when="+shared")
         # use pkg-config (support added in libxsmm-1.10) to link to libxsmm
         depends_on("pkgconfig", type="build")
 
